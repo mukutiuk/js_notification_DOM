b6105ea981f6393f2088ca32693aea19d4af4898
Roma <roma150269@gmail.com>
2024-06-06 14:37:41 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const div = document.createElement("div");
+    div.classList.add(`notification`);
+    div.classList.add(`${type}`);
+    div.style.top = `${posTop}px`;
+    div.style.right = `${posRight}px`;
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = title;
+    titleElement.classList.add("title");
+    div.appendChild(titleElement);
+    const text = document.createElement("p");
+    text.textContent = description;
+    div.appendChild(text);
+    document.body.appendChild(div);
+};
+const deletMeessage = ()=>{
+    const div = document.querySelectorAll("div");
+    div.forEach((i)=>i.style.display = "none");
+};
+setTimeout(deletMeessage, 2000);
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
